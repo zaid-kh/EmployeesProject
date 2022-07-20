a590db7ced2819344afda07aba2ef64c7acadb5f
zaid-kh <z.kh2800@gmail.com>
2022-07-20 14:50:32 +0300

    Add project files.

---
+++ EmployeesProject/Controllers/Employees.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesProject.Controllers
+{
+    public class Employees : Controller
+    {
+        public IActionResult Index() // display View Having Table
+        {
+            return View();
+        }
+        public IActionResult Create() // Display Form For Insert
+        {
+            return View();
+        }
+
+    }
+}
